4b2529f55fe8f36018ef56aa4916f47d4cca0446
den987655 <den987655@gmail.com>
2022-01-30 00:04:09 +0300

    add sites

---
+++ main.js
@@ -0,0 +1,11 @@
+(function (){
+  const burgerItem = document.querySelector('.burger');
+  const menu = document.querySelector('.header__navigation');
+  const menuCloseItem = document.querySelector('.header__nav-close');
+  burgerItem.addEventListener('click', () => {
+    menu.classList.add('header__navigation_active');
+  });
+    menuCloseItem.addEventListener('click', () => {
+    menu.classList.remove('header__navigation_active');
+  });
+}());
